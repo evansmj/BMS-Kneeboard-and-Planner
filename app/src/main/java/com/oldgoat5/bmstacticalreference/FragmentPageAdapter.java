c25f76424cb81115f91a9796574e55f02c473f6e
Michael Evans <evansmj@dukes.jmu.edu>
2015-05-08 12:38:57 -0400

    added korea navigation to manifest.

---
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/FragmentPageAdapter.java
@@ -0,0 +1,42 @@
+package com.oldgoat5.bmstacticalreference;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FragmentPageAdapter extends FragmentPagerAdapter
+{
+    
+    public FragmentPageAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return new LoadCardFragment();
+            case 1:
+                return new LoadOutFragment();
+            case 2:
+                return new TacticalReferenceFragment();
+            case 3:
+                return new AircraftManualsFragment();
+            case 4:
+                return new NavigationChartsFragment();
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return 5;
+    }
+    
+}
---
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/OrdinanceObject.java
@@ -0,0 +1,93 @@
+package com.oldgoat5.bmstacticalreference;
+
+/*****************************************************************
+ * Contains a class for containing ordinance objects from the 
+ * BMSLoadDb ordinance table.
+ * 
+ * @author Michael Evans
+ * 
+ *****************************************************************/
+public class OrdinanceObject
+{
+    private String id;
+    private String name;
+    private String weight;
+    private String drag;
+    private String damage;
+    private String guidance;
+    private String range;
+    private String blast;
+    private String info;
+    
+    /*****************************************************************
+     * This class contains an ordinance object.
+     * 
+     * @param id
+     * @param name
+     * @param weight
+     * @param drag
+     * @param damage
+     * @param guidance
+     * @param range
+     * @param blast
+     * @param info
+     *****************************************************************/
+    public OrdinanceObject(String id, String name, String weight, String drag, 
+            String damage, String guidance, String range, String blast, String info)
+    {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+        this.drag = drag;
+        this.damage = damage;
+        this.guidance = guidance;
+        this.range = range;
+        this.blast = blast;
+        this.info = info;
+    }
+    
+    public String getId()
+    {
+        return this.id;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public String getWeight()
+    {
+        return this.weight;
+    }
+    
+    public String getDrag()
+    {
+        return this.drag;
+    }
+    
+    public String getDamage()
+    {
+        return this.damage;
+    }
+    
+    public String getGuidance()
+    {
+        return this.guidance;
+    }
+    
+    public String getRange()
+    {
+        return this.range;
+    }
+    
+    public String getBlast()
+    {
+        return this.blast;
+    }
+    
+    public String getInfo()
+    {
+        return this.info;
+    }
+}
