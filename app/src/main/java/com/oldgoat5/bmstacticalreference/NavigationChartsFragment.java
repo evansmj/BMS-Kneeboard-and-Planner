c0c3e5312da5ffde604e8c09a081b46c92be3fa4
Michael Evans <evansmj@dukes.jmu.edu>
2015-05-07 23:29:36 -0400

    added navigation charts fragment

---
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/NavigationChartsFragment.java
@@ -0,0 +1,52 @@
+package com.oldgoat5.bmstacticalreference;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/*********************************************************************
+ * @author Michael Evans
+ * @version 5/7/2015
+ *
+ * Contains charts for Korea.
+ *********************************************************************/
+public class NavigationChartsFragment extends Fragment
+{
+    Button koreaSelectButton;
+
+    View view;
+    public View OnCreateView(LayoutInflater inflater,
+        ViewGroup container, Bundle savedInstanceState)
+    {
+        view = inflater.inflate(
+                R.layout.loadout_fragment_layout, container, false);
+
+        koreaSelectButton = (Button) view.findViewById(R.id.korea_navigation_select_button);
+
+
+        koreaSelectButton.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                showKoreaNavigation();
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+    /*****************************************************************
+     * Starts the korea navigation activity.
+     *****************************************************************/
+    public void showKoreaNavigation()
+    {
+
+    }
+}
