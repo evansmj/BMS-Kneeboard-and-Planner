ea38be261b70cff1330dcbc05e1ad6be8552be00
Michael Evans <evansmj@dukes.jmu.edu>
2015-05-28 23:20:10 -0400

    added fragment pager adapter for bfb

--- app/src/main/java/com/oldgoat5/bmstacticalreference/BattleForBalkansNavigationActivity.java
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/BattleForBalkansNavigationActivity.java
@@ -2,6 +2,10 @@ package com.oldgoat5.bmstacticalreference;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
 
 /*********************************************************************
  * Airport information for falcon-online Battle For Balkans Theater.
@@ -9,8 +13,13 @@ import android.os.Bundle;
  * @author Michael Evans
  * @since 5/26/2015
  *********************************************************************/
-public class BattleForBalkansNavigationActivity extends Activity
+public class BattleForBalkansNavigationActivity extends FragmentActivity
 {
+    private BattleForBalkansNavigationFragmentPageAdapter fragmentPageAdapter;
+    private ArrayList<PagerItem> tabsList;
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
---
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/BattleForBalkansNavigationFragmentPageAdapter.java
@@ -0,0 +1,102 @@
+package com.oldgoat5.bmstacticalreference;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/*********************************************************************
+ * Fragment pager adapter for falcon-online.org Balkans theater.
+ *
+ * @author Michael Evans
+ * @since 5/28/2015
+ *********************************************************************/
+public class BattleForBalkansNavigationFragmentPageAdapter extends FragmentPagerAdapter
+{
+    private final int COUNT = 11;
+    private Context context;
+
+    public BattleForBalkansNavigationFragmentPageAdapter(FragmentManager fm, Context context)
+    {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position) //south korea, north korea, japan, china, russia
+        {
+            case 0:
+                return new SouthKoreaChartFragment();
+
+            case 1:
+                return new NorthKoreaChartFragment();
+
+            case 2:
+                return new JapanKoreaChartFragment();
+
+            case 3:
+                return new ChinaKoreaChartFragment();
+
+            case 4:
+                return new RussiaKoreaChartFragment();
+
+            default:
+                break;
+        }
+
+        return null;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        switch (position) //south korea, north korea, japan, china, russia
+        {
+            case 0:
+                return "Italy"; //blue
+
+            case 1:
+                return "Sicily"; //blue
+
+            case 2:
+                return "Greece"; //red
+
+            case 3:
+                return "Macedonia"; //red
+
+            case 4:
+                return "Albania"; //red
+
+            case 5:
+                return "Slovenia"; //blue
+
+            case 6:
+                return "Montenegro"; //red
+
+            case 7:
+                return "Serbia"; //red
+
+            case 8:
+                return "Bosnia Herzegovnia"; //red
+
+            case 9:
+                return "Croatia"; //gray
+
+            case 10:
+                return "Hungary"; //gray
+
+            default:
+                break;
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return COUNT;
+    }
+}
