8276a5caca4e341fa61a0d3e66ec31285545a0cd
Michael Evans <evansmj@dukes.jmu.edu>
2015-07-20 13:34:04 -0400

    zoom clickable charts added

--- app/src/main/java/com/oldgoat5/bmstacticalreference/navigation/balkans/GreeceBalkansChartFragment.java
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/navigation/balkans/GreeceBalkansChartFragment.java
@@ -1,5 +1,6 @@
 package com.oldgoat5.bmstacticalreference.navigation.balkans;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,8 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.oldgoat5.bmstacticalreference.R;
+import com.oldgoat5.bmstacticalreference.navigation.ZoomImageView;
 
 /********************************************************************
  * Copyright © Michael Evans - All Rights Reserved.
@@ -20,7 +23,10 @@ public class GreeceBalkansChartFragment extends Fragment
 {
     Context CONTEXT;
 
+    private Dialog dialog;
+    private ImageView imageView;
     private View view;
+    private ZoomImageView zoomImageView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
@@ -34,6 +40,23 @@ public class GreeceBalkansChartFragment extends Fragment
         {
             CONTEXT = getActivity();
         }
+
+        imageView = (ImageView) view.findViewById(R.id.greece_balkans_image_view);
+
+        dialog = new Dialog(CONTEXT);
+        zoomImageView = new ZoomImageView(CONTEXT);
+
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                zoomImageView.setImageResource(R.drawable.greece_airbases);
+                dialog.setContentView(zoomImageView);
+                dialog.setTitle("Greece Airbases");
+                dialog.show();
+            }
+        });
 
         return view;
     }
