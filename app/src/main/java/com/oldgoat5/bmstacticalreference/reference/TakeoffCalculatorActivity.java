bc37e06c93d9c67a1b3fcf7f67ed19b6deb4f36f
Michael Evans <evansmj@dukes.jmu.edu>
2016-03-06 21:46:37 -0500

    Add error checking to takeoff calculator

--- app/src/main/java/com/oldgoat5/bmstacticalreference/reference/TakeoffCalculatorActivity.java
+++ app/src/main/java/com/oldgoat5/bmstacticalreference/reference/TakeoffCalculatorActivity.java
@@ -2,6 +2,7 @@ package com.oldgoat5.bmstacticalreference.reference;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.text.Editable;
@@ -16,6 +17,7 @@ import com.oldgoat5.bmstacticalreference.R;
 
 import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math3.exception.OutOfRangeException;
 
 /********************************************************************
  * Copyright © Michael Evans - All Rights Reserved.
@@ -33,12 +35,16 @@ public class TakeoffCalculatorActivity extends Activity
             142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176,
             178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208};
 
+    private final int minWeight = 17900;
+    private final int maxWeight = 48000;
+
     private Button saveToDataCardButton;
     private EditText grossWeightEditText;
     private PolynomialSplineFunction takeoffSpeedFunction;
     private PowerSettingEnum selectedPowerSetting;
     private RadioGroup takeoffPowerRadioGroup;
     private SplineInterpolator interpolator;
+    private TextView grossWeightLabelTextView;
     private TextView rotateResultTextView;
     private TextView savedStatusTextView;
     private TextView takeoffSpeedResultTextView;
@@ -65,6 +71,9 @@ public class TakeoffCalculatorActivity extends Activity
         //get takeoff speed
         calculatedTakeoffSpeed = takeoffSpeedFunction.value(selectedGrossWeight);
 
+        grossWeightLabelTextView.setTextColor(
+                ContextCompat.getColor(getApplicationContext(), R.color.gray));
+
         //apply rotate to takeoff speed
         switch (selectedPowerSetting)
         {
@@ -80,13 +89,27 @@ public class TakeoffCalculatorActivity extends Activity
         //report results in formatted strings
         rotateResultTextView.setText(
                 String.format(getApplicationContext().getString(
-                        R.string.takeoff_speed_result),
+                                R.string.takeoff_speed_result),
                         (int) calculatedRotateSpeed));
 
         takeoffSpeedResultTextView.setText(
                 String.format(getApplicationContext().getString(
-                        R.string.takeoff_speed_result),
+                                R.string.takeoff_speed_result),
                         (int) calculatedTakeoffSpeed));
+
+    }
+
+    /*****************************************************************
+     * Handles when there is invalid input
+     ****************************************************************/
+    private void invalidate()
+    {
+        grossWeightLabelTextView.setTextColor(Color.RED);
+        calculatedRotateSpeed = 0;
+        calculatedTakeoffSpeed = 0;
+        selectedGrossWeight = 0;
+        rotateResultTextView.setText("");
+        takeoffSpeedResultTextView.setText("");
     }
 
     /*****************************************************************
@@ -107,6 +130,8 @@ public class TakeoffCalculatorActivity extends Activity
         takeoffPowerRadioGroup = (RadioGroup) findViewById(
                 R.id.takeoff_calculator_takeoff_power_radio_group);
 
+        grossWeightLabelTextView = (TextView) findViewById(
+                R.id.takeoff_calculator_gross_weight_description);
         rotateResultTextView = (TextView) findViewById(
                 R.id.takeoff_calculator_rotate_result_text_view);
         savedStatusTextView = (TextView) findViewById(
@@ -139,17 +164,21 @@ public class TakeoffCalculatorActivity extends Activity
             {
                 try
                 {
-                    double weight = Double.parseDouble(editable.toString());
+                    int weight = Integer.parseInt(editable.toString());
 
-                    if ((weight >= 17900) && (weight <= 48000))
+                    if ((weight >= minWeight) && (weight <= maxWeight))
                     {
-                        selectedGrossWeight = Integer.parseInt(editable.toString());
+                        selectedGrossWeight = weight;
                         calculateTakeoffSpeeds();
+                    }
+                    else
+                    {
+                        invalidate();
                     }
                 }
                 catch (NumberFormatException e)
                 {
-                    selectedGrossWeight = 0;
+                    invalidate();
                 }
 
             }
@@ -220,7 +249,22 @@ public class TakeoffCalculatorActivity extends Activity
                         break;
                 }
 
-                calculateTakeoffSpeeds();
+                try
+                {
+                    int weight = Integer.parseInt(grossWeightEditText.getText().toString());
+                    if (weight >= 17900 && weight <= 48000)
+                        calculateTakeoffSpeeds();
+                    else
+                        throw new OutOfRangeException(weight, minWeight, maxWeight);
+                }
+                catch (NumberFormatException e)
+                {
+                    //do nothing if user just switched, so red doesn't show up for nothing.
+                }
+                catch (OutOfRangeException e)
+                {
+                    invalidate();
+                }
             }
         });
     }
